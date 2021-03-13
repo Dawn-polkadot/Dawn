62c492e43567895c944ab4b9ed72ba6bd6389c10
Jackieyewang <jakiewangxy@gmail.com>
2021-03-14 00:31:40 +0800

    frontend

---
+++ src/utils/utils.ts
@@ -0,0 +1,19 @@
+export const ApiProxyUri: string = '/nft-api';
+
+export const categoryList: CategoryType[] = [
+  {
+    id: 0,
+    name: 'All',
+    position: 1,
+  },
+  {
+    id: 1,
+    name: '🌈 Art',
+    position: 2,
+  },
+  {
+    id: 2,
+    name: '🕹 Games',
+    position: 3,
+  },
+];
